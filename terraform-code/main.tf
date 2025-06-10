018ba0bc28718531085338e35a540125060ab402
Tam O'Donnell <47383699+ThomasHAOD@users.noreply.github.com>
2025-06-10 12:00:02 +0100

    lesson 36 - deployment in action

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -45,11 +45,11 @@ resource "github_repository_file" "index" {
   overwrite_on_create = true
 }
 
-# output "clone-urls" {
-#   value       = { for i in github_repository.mtc-repo[*] : i.name => i.http_clone_url }
-#   description = "repository name and url"
-#   sensitive   = false
-# }
+output "clone-urls" {
+  value       = { for i in github_repository.mtc-repo : i.name => [i.http_clone_url, i.ssh_clone_url] }
+  description = "repository name and url"
+  sensitive   = false
+}
 
 # output "varsource" {
 #   value       = var.varsource
