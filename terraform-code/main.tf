3583bccc907cf4836acb32779de4087bd1832dfc
Tam O'Donnell <47383699+ThomasHAOD@users.noreply.github.com>
2025-06-12 07:20:47 +0100

    lesson 50 deploy key

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -23,11 +23,15 @@ module "repos" {
 }
 
 module "deploy-key" {
-  for_each = local.repos
+  for_each = toset(flatten([ for k, v in module.repos : keys(v.clone-urls) if k == "dev" ]))
   source = "./modules/deploy-key"
   repo_name = each.key
 }
 
 output "repo-info" {
   value = { for k, v in module.repos : k => v.clone-urls }
+}
+
+output "repo-list" {
+    value = flatten([ for k, v in module.repos : keys(v.clone-urls) if k == "dev" ])
 }
